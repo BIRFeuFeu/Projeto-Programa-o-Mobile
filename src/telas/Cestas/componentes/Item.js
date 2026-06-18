bc71b7b7c49c79bb52ac0d551bf6448480bb15c4
BIRFeuFeu <alfeu.paula@escola.pr.gov.br>
2026-06-18 08:52:17 -0300

    Update Item.js

--- src/telas/Cestas/componentes/Item.js
+++ src/telas/Cestas/componentes/Item.js
@@ -1,67 +1,32 @@
 import React from 'react';
-import { Image, StyleSheet, View, TouchableOpacity } from 'react-native';
+import { View, Image, StyleSheet, FlatList } from 'react-native';
 
 import Texto from '../../../componentes/Texto';
 
-export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, preco, botao }) {
-  return <>
+export default function Item({ item: { nome, imagem } }) {
+  return <View style={estilos.item}>
+    <Image source={imagem} style={estilos.imagem}/>
     <Texto style={estilos.nome}>{ nome }</Texto>
-    <View style={estilos.fazenda}>
-      <Image source={logoFazenda} style={estilos.imagemFazenda} />
-      <Texto style={estilos.nomeFazenda}>{ nomeFazenda }</Texto>
-    </View>
-    <Texto style={estilos.descricao}>{ descricao }</Texto>
-    <Texto style={estilos.preco}>{ preco }</Texto>
-
-    <TouchableOpacity style={estilos.botao} onPress={() => {}}>
-      <Texto style={estilos.textoBotao}>{ botao }</Texto>
-    </TouchableOpacity>
-  </>
+  </View>
 }
 
 const estilos = StyleSheet.create({
-  nome: {
-    color: "#464646",
-    fontSize: 26,
-    lineHeight: 42,
-    fontWeight: 'bold',
-  },
-  fazenda: {
+  item: {
     flexDirection: "row",
-    paddingVertical: 12,
-  },
-  imagemFazenda: {
-    width: 32,
-    height: 32,
-  },
-  nomeFazenda: {
-    fontSize: 16,
-    lineHeight: 26,
-    marginLeft: 12,
-  },
-  descricao: {
-    color: "#A3A3A3",
-    fontSize: 16,
-    lineHeight: 26,
-  },
-  preco: {
-    color: "#2A9F85",
-    fontWeight: "bold",
-    fontSize: 26,
-    lineHeight: 42,
-    marginTop: 8,
-  },
-  botao: {
-    marginTop: 16,
-    backgroundColor: "#2A9F85",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ECECEC",
     paddingVertical: 16,
-    borderRadius: 6,
+    marginHorizontal: 16,
+    alignItems: "center",
   },
-  textoBotao: {
-    textAlign: "center",
-    color: "#ffffff",
+  imagem: {
+    width: 46,
+    height: 46,
+  },
+  nome: {
     fontSize: 16,
     lineHeight: 26,
-    fontWeight: "bold",
+    marginLeft: 11,
+    color: "#464646"
   },
-})
+});
